a136fd1c9d00fb147d707eefdd6d2740117558e9
MaharTariq <mahartariq499@gmail.com>
2020-06-03 21:45:41 +0500

    reactapp development

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+function App(){
+  const SayHello = () =>{
+    //console.log('I Just Fell in Love with REACT');
+    alert('I just fell in love with REACT')
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return(
+   <div>
+     <h1>Hello React</h1>
+     <button onClick={SayHello}>Annoucement</button>
+   </div>
+ );
 }
 
 export default App;
